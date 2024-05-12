eb39ab3fcf12e7e556a533e72646ee31a6b01e07
Viktor Kolesnikov <viktorkolesnikov@Viktors-MacBook-Air.local>
2024-05-12 21:27:46 +0900

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,265 @@
-import logo from './logo.svg';
 import './App.css';
+import chatgptimage from './module1.jpg'
+import chatgptimage2 from './picture2.jpeg'
+import sergey from './sergey.png'
+import alice from './alice.jpeg'
+import chatgpt from './chatgpt.png'
+import suno from './suno.png'
+import crewai from './crewai.png'
+import colab from './colab.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="bg-neutral-900 p-3">
+
+      
+    <body className='body' class="leading-normal tracking-normal text-indigo-400 bg-cover bg-fixed">
+    <div class="h-full">
+      <div class="w-full container mx-auto">
+        <div class="w-full flex items-center justify-between pt-3">
+          <a class="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+            WeEarn<span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">OnAI</span>
+          </a>
+
+  
+        </div>
+      </div>
+
+      <div class="container pt-24 md:pt-30 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+
+        <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+          <h1 class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+            <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+              Зарабатывай c помощью AI
+            </span>
+          </h1>
+          <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left text-white">
+            Уже завтра ты сможешь заработать первые деньги на chatGPT, Suno и другом
+          </p>
+
+            <div class="flex items-center justify-center pt-4">
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+<span class="relative px-20 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+Купить
+</span>
+</button>
+            </div>
+        </div>
+
+
+        <div class="w-full xl:w-3/5 p-12 overflow-hidden">
+          <img class="mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6 outline shadow-md" src={chatgptimage} />
+        </div>
+
+
+
+
+<div class="py-16 ">
+  <div class="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-16">
+    <div class="lg:bg-neutral-900 dark:lg:bg-darker lg:p-16 rounded-[4rem] space-y-6 md:flex flex-row-reverse md:gap-6 justify-center md:space-y-0 lg:items-center">
+      <div class="md:5/12 lg:w-1/2">
+        <img
+          src={chatgptimage2}
+          alt="image"
+          loading="lazy"
+          width=""
+          height=""
+          className='outline'
+        />
+      </div>
+      <div class="md:7/12 lg:w-1/2">
+        <h2 class="text-3xl font-bold text-gray-900 md:text-4xl dark:text-white">
+          Мы исследуем схемы заработка
+        </h2>
+        <p class="my-8 text-gray-600 dark:text-gray-300">
+          Хотите понять, как сейчас на автомате зарабатывают на западе? Каким образом с помощью chatGPT заменяют рабочий труд? Тогда это к нам, наши гайды, схемы позволят вам начать зарабатывать уже завтра
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div class="divide-y space-y-4 divide-gray-100 dark:divide-gray-800">
+          <div class="mt-8 flex gap-4 md:items-center">
+            <div class="w-12 h-12 flex gap-4 rounded-full bg-indigo-100 dark:bg-indigo-900/20">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-indigo-500 dark:text-indigo-400">
+                <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z" clip-rule="evenodd" />
+              </svg>        
+            </div>
+            <div class="w-5/6">
+              <h4 class="font-semibold text-lg text-gray-700 dark:text-indigo-300">Поддержка в ТГ</h4>
+              <p class="text-gray-500 dark:text-gray-400">Вы вступите в телеграмм канал, где общаются другие участники и зарабатывают в закрытом сообществе</p>
+            </div> 
+          </div> 
+          <div class="pt-4 flex gap-4 md:items-center">
+            <div class="w-12 h-12 flex gap-4 rounded-full bg-teal-100 dark:bg-teal-900/20">  
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-teal-600 dark:text-teal-400">
+                <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+              </svg>                                      
+            </div>
+            <div class="w-5/6">
+              <h4 class="font-semibold text-lg text-gray-700 dark:text-teal-300">Технологии</h4>
+              <p class="text-gray-500 dark:text-gray-400">Мы научим вас применять ИИ, даже если вы совсем профан</p>
+            </div> 
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+        
+
+
+
+        <div class="bg-neutral-900">
+    <div class="container m-auto px-6 py-20 md:px-12 lg:px-20">
+        <div class="m-auto text-center lg:w-8/12 xl:w-7/12">
+            <h2 class="text-2xl text-white font-bold md:text-4xl">Что вы получаете?</h2>
+        </div>
+        <div class="mt-12 m-auto -space-y-4 items-center justify-center md:flex md:space-y-0 md:-space-x-4 xl:w-10/12">
+            <div class="relative z-10 -mx-4 group md:w-6/12 md:mx-0 lg:w-5/12">
+                <div aria-hidden="true" class="absolute top-0 w-full h-full rounded-2xl bg-white shadow-xl transition duration-500 group-hover:scale-105 lg:group-hover:scale-110"></div>
+                <div class="relative p-6 space-y-6 lg:p-8">
+                    <h3 class="text-3xl text-gray-700 font-semibold text-center">Пакет</h3>
+                    <div>
+                        <div class="relative flex justify-around">
+                            <div class="flex items-end">
+                                <span class="text-8xl text-gray-800 font-bold leading-0">499</span>
+                                <div class="pb-2">
+<span class="block text-2xl text-gray-700 font-bold">₽</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <ul role="list" class="w-max space-y-4 py-6 m-auto text-gray-600">
+                        <li class="space-x-2">
+                            <span class="text-purple-500 font-semibold">✓</span>
+                            <span>100+ схем заработка</span>
+                        </li>
+                        <li class="space-x-2">
+                            <span class="text-purple-500 font-semibold">✓</span>
+                            <span>Пополнение каждый месяц</span>
+                        </li>
+                        <li class="space-x-2">
+                            <span class="text-purple-500 font-semibold">✓</span>
+                            <span>Доступ в закрытый канал</span>
+                        </li>
+                    </ul>
+                    <p class="flex items-center justify-center space-x-4 text-lg text-gray-600 text-center">
+                    </p>
+                    <button type="submit" title="Submit" class="block w-full py-3 px-6 text-center rounded-xl transition bg-green-400 hover:bg-green-600">
+                        <span class="text-white font-semibold">
+                            Купить
+                        </span>
+                    </button>
+                </div>
+            </div>
+
+            <div class="relative group md:w-6/12 lg:w-7/12">
+                <div aria-hidden="true" class="absolute top-0 w-full h-full rounded-2xl bg-white shadow-lg transition duration-500 group-hover:scale-105"></div>
+                <div class="relative p-6 pt-16 md:p-8 md:pl-12 md:rounded-r-2xl lg:pl-20 lg:p-16">
+                    <ul role="list" class="space-y-4 py-6 text-gray-600">
+                        <li class="space-x-2">
+                            <span class="text-purple-500 font-semibold">✓</span>
+                            <span>Каждый день новые идеи</span>
+                        </li>
+                        <li class="space-x-2">
+                            <span class="text-purple-500 font-semibold">✓</span>
+                            <span>Способы реализации</span>
+                        </li>
+                        <li class="space-x-2">
+                            <span class="text-purple-500 font-semibold">✓</span>
+                            <span>Инсайды из корпораций</span>
+                        </li>
+                        <li class="space-x-2">
+                            <span class="text-purple-500 font-semibold">✓</span>
+                            <span>Советы, помощь, ответы на вопросы</span>
+                        </li>
+                    </ul>
+                    <p class="text-gray-700">Купив доступ, вы пожизненно получаете каждодневные гайды, информацию, схемы заработка с помощью AI.</p>
+                    <div class="mt-6 flex justify-between gap-6">
+                        <img class="w-16 lg:w-16" src={chatgpt} loading="lazy" alt="airbnb"/>
+                        <img class="w-16 lg:w-16" src={suno} loading="lazy" alt="bissell"/>
+                        <img class="w-16 lg:w-16" src={crewai} loading="lazy" alt="ge"/>
+                        <img class="w-16 lg:w-16" src={colab} loading="lazy" alt="microsoft"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<div class="py-16">
+  <div class="xl:container m-auto px-6 text-gray-600 dark:text-gray-300 md:px-12 xl:px-6">
+    <h2 class="mb-12 text-center text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
+      Что говорят наши клиенты
+    </h2>
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper pb-6">
+        <div class="swiper-slide !bg-transparent">
+          <div class="mx-auto space-y-6 text-center md:w-8/12 lg:w-7/12">
+            <img
+              class="mx-auto !h-16 !w-16 rounded-full"
+              src={sergey}
+              alt="user avatar"
+              height="220"
+              width="220"
+              loading="lazy"
+            />
+            <p>
+              <span class="font-serif">"</span> В первый же день получил десятки схем, опробовал парочку, потратил несколько часов, результат на лицо 50-100 долларов в день ничего не делая. <span class="font-serif">"</span>
+            </p>
+            <div>
+              <h6 class="text-lg font-semibold leading-none">Егор Ефремов</h6>
+            </div>
+          </div>
+        </div>
+
+        <div class="swiper-slide !bg-transparent pt-16">
+          <div class="mx-auto space-y-6 text-center md:w-8/12 lg:w-7/12">
+            <img
+              class="mx-auto !h-16 !w-16 rounded-full"
+              src={alice}
+              alt="user avatar"
+              height="220"
+              width="220"
+              loading="lazy"
+            />
+            <p>
+              <span class="font-serif">"</span> Думала, что очередной скучный гайд, оказалось все куда интереснее - закрытое сообщество, инсайды, ответы на вопросы и полная поддержка! <span class="font-serif">"</span>
+            </p>
+            <div>
+              <h6 class="text-lg font-semibold leading-none">Алиса Сахарова</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="swiper-pagination"></div>
+    </div>
+  </div>
+</div>
+                                    
+
+
+
+                                    
+
+
+
+
+  
+        <div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+          <a class="text-gray-500 no-underline hover:no-underline" href="#">&copy; 2024 </a>
+
+          <a class="text-gray-500 no-underline hover:no-underline" href="https://weearnonai.ru">weearnonai.ru</a>
+        </div>
+      </div>
+    </div>
+
+  </body>
     </div>
   );
 }
